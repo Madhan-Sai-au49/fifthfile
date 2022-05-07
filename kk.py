4426086eb9f31ee028ca604d65f58fbbc0ef6501
Madhan-sai-au46 <104922258+Madhan-sai-au46@users.noreply.github.com>
2022-05-07 14:36:37 +0530

    Update kk.py

--- kk.py
+++ kk.py
@@ -1,3 +1,15 @@
-a = 100
-b = 100
-c = (a+b)
+#HIGHEST AMONG THREE NUMBERS..
+
+m = input("ENTER A NUMBER = M : ")
+n = input("ENTER A NUMBER = N : ")
+o = input("ENTER A NUMBER = 0 : ")
+p = input("ENTER A NUMBER = P : ")
+
+if (m>n and m>o and m>p):
+    print(" M IS GREATER THAN N,O,P")
+elif (n>m and n>o and n>p):
+    print("N IS GREATER THAN M,O,P")
+elif (o>m and o>n and o>p):
+    print("O IS GREATER THAN M,N,P")
+else:
+    print("P IS GREATER THAN M,N,O")
